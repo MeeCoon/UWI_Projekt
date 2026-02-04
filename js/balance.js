158e904e46f32c088c8b71d353f62c97530971cd
MeeCoon <melvin.haueter@student.kvbl.ch>
2026-02-04 08:55:34 +0100

    Create balance.js

---
+++ js/balance.js
@@ -0,0 +1,13 @@
+// ======== ZENTRALE BILANZ-DATEN ========
+let bilanz = {
+  2024: {
+    aktiven: {
+      1000: { name: "Kasse", wert: 0 },
+      1020: { name: "Bankguthaben", wert: 0 }
+    },
+    passiven: {
+      2800: { name: "Eigenkapital", wert: 0 },
+      2000: { name: "Verbindlichkeiten", wert: 0 }
+    }
+  }
+};
